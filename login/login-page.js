79dd06a6838e817963a6164f48783f47619f6a88
Glass-paper <glasspapereth@gmail.com>
2021-10-04 12:31:22 +0530

    UI

--- login/login-page.js
+++ login/login-page.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
-         window.location.replace("file:///C:/Users/SABYASACHI/Desktop/GlassPaper/login/option.html")
+         window.location.replace("./option.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
